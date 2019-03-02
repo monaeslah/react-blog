256684264cc70cb7add6669620f8c426e33a76d8
mona eslahkonha <46243271+monaeslah@users.noreply.github.com>
2019-03-02 19:17:31 +0330

    chenged essay for auther type&unipc password in user.js

--- models/Essay.js
+++ models/Essay.js
@@ -1,13 +1,17 @@
-const mongos=require("mongoose");
-const Schema=mongos.Schema;
-const essay=new Schema({
-    
-   title:String,
-   description:String,
-   author: String,
+const mongos = require("mongoose");
+const Schema = mongos.Schema;
+const essay = new Schema({
+
+   title: String,
+   description: String,
+   author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+   },
    Date: Date,
    shorttext: String,
-image:String
+   image: String
 
 });
-module.exports=mongos.model('essay',essay);
+module.exports = mongos.model('essay', essay);
--- models/User.js
+++ models/User.js
@@ -21,11 +21,11 @@ email: {
     type: String,
     required: true
 
-   
+
 },
-    // date: {
-    //     type:Date,
-    //     default:Date.now
-    // }
+    date: {
+        type:Date,
+        default:Date.now()
+    }
 })
 module.exports = User = mongoose.model('users', UserSchema)
